f226caaf26cd41daf8afd5bd5cd61ae779be5c60
ehdtjs0612 <inko51366@naver.com>
2024-10-31 19:57:27 +0900

    feat: define routes

--- network/root.go
+++ network/root.go
@@ -15,9 +15,27 @@ func NewNetwork() *Network {
 		engin: gin.New(),
 	}
 
+	newUserRouter(n)
+
 	return n
 }
 
 func (n *Network) ServerStart(port string) error {
 	return n.engin.Run(port)
+}
+
+func (n *Network) registerGET(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.GET(path, handler...)
+}
+
+func (n *Network) registerPOST(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.POST(path, handler...)
+}
+
+func (n *Network) registerPUT(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.PUT(path, handler...)
+}
+
+func (n *Network) registerDELETE(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.DELETE(path, handler...)
 }
